779a07ec90e6ca2c19e7da5edde87daa3f8b2ba2
karthik karavatt <karthikkaravatt@gmail.com>
2023-05-13 12:29:09 +0800

    Added the ability to evaluate the file and add to the results

--- main.c
+++ main.c
@@ -31,7 +31,65 @@ int listNumberOfFiles(char *directoryName) {
   closedir(dirp);
   return files;
 }
+void printFile(char *filename) {
+  char buf;
+  int file = openFile(filename, O_RDWR | O_RDONLY);
+  int counter = 0;
+  // reads the file one character at a time
+  while (read(file, &buf, 1)) {
+    printf("%c", buf);
+    counter++;
+  }
+  close(file);
+}
+void evaluate_file(char *filename) {
+  int file1 = open(filename, O_RDONLY);
+  char buffer[1000];
+  int fileRead = read(file1, buffer, sizeof(buffer));
+  close(file1);
+  char *token = strtok(buffer, "\n");
+  token = strtok(NULL, "\n");
+  int operand1 = atoi(token);
+  token = strtok(NULL, "\n");
+  char operator= token[0];
+  token = strtok(NULL, "\n");
+  int operand2 = atoi(token);
+  int result;
+  switch (operator) {
+  case '+':
+    result = operand1 + operand2;
+    break;
+  case '-':
+    result = operand1 - operand2;
+    break;
+  case '*':
+    result = operand1 * operand2;
+    break;
+  case '/':
+    result = operand1 / operand2;
+    break;
+  }
+  int file2 = open("results", O_WRONLY | O_CREAT | O_APPEND, 0644);
+  if (result < 0) {
+    write(file2, "-", 1);
+    result = -result;
+  }
+  int num_digits = 0;
+  int temp_result = result;
+  do {
+    num_digits++;
+    temp_result /= 10;
+  } while (temp_result > 0);
 
+  char result_str[num_digits];
+  for (int i = num_digits - 1; i >= 0; i--) {
+    result_str[i] = '0' + (result % 10);
+    result /= 10;
+  }
+  write(file2, result_str, num_digits);
+  write(file2, "\n", 1);
+  close(file2);
+}
 char **listFiles(int numberOfFiles, char *directoryName) {
   char **files = malloc(sizeof(char *) * numberOfFiles);
   struct dirent *dp;
@@ -42,9 +100,10 @@ char **listFiles(int numberOfFiles, char *directoryName) {
 
     char *foundFile = strrchr(dp->d_name, '.');
     if (foundFile && strcmp(foundFile, ".usp") == 0) {
-        files[counter] = malloc(sizeof(char)*256);
-        strcpy(files[counter], dp->d_name);
-        counter++;
+      // dp name has a max of 256 characters
+      files[counter] = malloc(sizeof(char) * 256);
+      strcpy(files[counter], dp->d_name);
+      counter++;
     }
     dp = readdir(dirp);
   }
@@ -52,13 +111,13 @@ char **listFiles(int numberOfFiles, char *directoryName) {
   return files;
 }
 int main(void) {
-    int numberOfFiles = listNumberOfFiles("./"); 
-    char** files = listFiles(numberOfFiles , "./");
-    for (int i = 0; i < numberOfFiles; i++) {
-        printf("%s\n", files[i]);
-    }
-    for (int i = 0; i < numberOfFiles; i++) {
-        free(files[i]);
-    }
-    free(files);
+  int numberOfFiles = listNumberOfFiles("./");
+  char **files = listFiles(numberOfFiles, "./");
+  for (int i = 0; i < numberOfFiles; i++) {
+    evaluate_file(files[i]);
+  }
+  for (int i = 0; i < numberOfFiles; i++) {
+    free(files[i]);
+  }
+  free(files);
 }
